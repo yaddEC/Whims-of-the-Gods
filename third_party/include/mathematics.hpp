be8829edff5ac14b17901ffce4556f318304f979
y.dechaux <y.dechaux@student.isartdigital.com>
2021-11-23 14:33:50 +0100

    1ere librarie math

---
+++ third_party/include/mathematics.hpp
@@ -0,0 +1,19 @@
+#pragma once 
+#include <cmath>
+template<typename A>
+inline float norm(A Vec)
+{
+return sqrt((Vec.x*Vec.x)+(Vec.y*Vec.y));
+}
+
+template<typename A, typename B>
+bool collCirclex2(A posCircle1, int radiusCircle1, B posCircle2,int radiusCircle2)
+{
+    A centerCenterVector = {posCircle1.x - posCircle2.x, posCircle1.y - posCircle2.y};
+    float normCenterCenterVector = norm(centerCenterVector);
+    if (normCenterCenterVector <= radiusCircle1 + radiusCircle2)
+    {
+        return true;
+    }
+    return false;
+}
