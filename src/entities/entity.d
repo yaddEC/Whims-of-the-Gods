02688c314fe601cc3bd18b511965282638e3466f
Matias HENO <m.heno@DELL-3240S-04-L.ISART.LAN>
2021-11-30 15:45:28 +0100

    tiny bug fix

--- src/entities/entity.d
+++
@@ -1,4 +0,0 @@
-src/entities/entity.o: src/entities/entity.cpp src/entities/enemy.hpp \
- src/entities/entity.hpp third_party/include/raylib.h \
- third_party/include/mathematics.hpp src/entities/turret.hpp \
- src/entities/tower.hpp
