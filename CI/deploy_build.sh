d4cbc1ea629b1df0e4dab8edc2bf9a9e94cdfa01
j.chaocatala <j.chaocatala@student.isartdigital.com>
2021-11-30 14:27:00 +0000

    Update deploy_build.sh

--- CI/deploy_build.sh
+++ CI/deploy_build.sh
@@ -1,1 +1,41 @@
+#!/bin/bash
 
+# show environment variables
+set
+set -x
+
+echo "deploying to server, please be patient..."
+
+base_file_name=$1
+deployed_files=()
+
+echo $base_file_name
+
+mkdir -p $base_file_name
+
+# add correct files for the type of build requested
+if [ $base_file_name == 'linux_build' ] 
+then
+	cp -r tower assets/ $base_file_name
+	zip -r $base_file_name.zip $base_file_name
+	deployed_files+=($base_file_name.zip)
+elif [ $base_file_name == 'windows_build' ] 
+then
+	cp -r tower.exe assets/ $base_file_name
+	zip -r $base_file_name.zip $base_file_name
+	deployed_files+=($base_file_name.zip)
+elif [ $base_file_name == 'emscripten_build' ] 
+then
+	cp -r tower.js tower.html tower.data tower.wasm $base_file_name
+	deployed_files+=($base_file_name)
+fi
+
+# print our contents of files added 
+echo ${deployed_files[*]}
+
+# deploy zip to deploy folder
+deploy_folder=~/public_html/testdir/${CI_COMMIT_TIMESTAMP:-no_date}_${CI_COMMIT_SHORT_SHA:-no_hash}
+
+mkdir -p $deploy_folder
+
+cp -r ${deployed_files[*]} $deploy_folder
